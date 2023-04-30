3971e31e3c7b4e6231be022ec10b3841c05d2851
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2023-04-30 11:32:17 +0100

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -48,3 +48,16 @@ var fizzBuzz = function(n) {
         }
         return output
     }
+
+// Solution 2:
+
+var fizzBuzz = function(n) {
+    let output = []
+        for(let i = 1; i <= n; i++){
+            if(i % 15 === 0) output.push('FizzBuzz')
+            else if(i % 3 === 0) output.push('Fizz')
+            else if(i % 5 === 0) output.push('Buzz')
+            else output.push(String(i))
+        }
+        return output
+    }
