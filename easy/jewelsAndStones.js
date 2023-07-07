ca57cf3eb7935659c415b9ff2bd62490c310bc7b
Andy Learmouth <andymlearmouth@hotmail.com>
2023-07-07 12:47:31 +0100

    Jewels and Stones Solution 2: Quadratic loops

--- easy/jewelsAndStones.js
+++ easy/jewelsAndStones.js
@@ -47,3 +47,17 @@ var numJewelsInStones = function(jewels, stones) {
     }
     return count
 };
+
+// Solution 2:
+
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0
+    for(let i = 0; i < jewels.length; i++){
+        for(let j = 0; j < stones.length; j++){
+            if(jewels[i] === stones[j]){
+                count++
+            }
+        }
+    }
+    return count
+};
