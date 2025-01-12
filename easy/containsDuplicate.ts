ab901c4a6e82ec1f34d0d49cb8f3374fd0ff86dd
Andy Learmouth <andymlearmouth@hotmail.com>
2025-01-12 17:06:31 +0000

    Find duplicates S3.

--- easy/containsDuplicate.ts
+++ easy/containsDuplicate.ts
@@ -64,3 +64,16 @@ function containsDuplicate(nums: number[]): boolean {
   }
   return false;
 }
+
+// Solution 3: Too slow to pass as it times out on LC but it would work given more time
+
+function containsDuplicate(nums: number[]): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
