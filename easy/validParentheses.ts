f41aa4f43cb43fe40cb3b01f33a9611a9a819d24
Andy Learmouth <andymlearmouth@hotmail.com>
2025-01-20 17:39:00 +0000

    Solution 2 for valid parenthesis using a stack and hashmap.

--- easy/validParentheses.ts
+++ easy/validParentheses.ts
@@ -44,7 +44,30 @@ function isValid(s: string): boolean {
 };
 */
 
-// Solution: 1: Brute force using replace()
+// Solution 1: Stack with a hashmap:
+
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  const closeToOpen: { [key: string]: string } = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  for (let c of s) {
+    if (closeToOpen[c]) {
+      if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(c);
+    }
+  }
+  return stack.length === 0;
+}
+
+// Solution: 2: Brute force using replace()
 
 function isValid(s: string): boolean {
   while (s.includes("()") || s.includes("{}") || s.includes("[]")) {
