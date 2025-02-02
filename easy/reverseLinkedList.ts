6355b0a303acb2cb0ea6b4299af0b4808f4e736f
Andy Learmouth <andymlearmouth@hotmail.com>
2025-02-02 18:39:00 +0000

    Merge two sorted lists LC Solution 1: Iterative.

--- easy/reverseLinkedList.ts
+++ easy/reverseLinkedList.ts
@@ -25,6 +25,17 @@ Follow up: A linked list can be reversed either iteratively or recursively. Coul
 
 Starter code:
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+
 function reverseList(head: ListNode | null): ListNode | null {
     
 };
