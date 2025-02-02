a43c62e725340ca115919aae9ce8b91cb1c3dcb2
Andy Learmouth <andymlearmouth@hotmail.com>
2025-02-02 18:39:00 +0000

    Merge two sorted lists LC Solution 1: Iterative.

--- easy/mergeTwoSortedLists.ts
+++ easy/mergeTwoSortedLists.ts
@@ -42,3 +42,32 @@ Starter code:
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     
 };
+
+*/
+
+// Solution: Iterative;
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  const dummy = { val: 0, next: null };
+  let node = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      node.next = list1;
+      list1 = list1.next;
+    } else {
+      node.next = list2;
+      list2 = list2.next;
+    }
+    node = node.next;
+  }
+  if (list1) {
+    node.next = list1;
+  } else {
+    node.next = list2;
+  }
+  return dummy.next;
+}
